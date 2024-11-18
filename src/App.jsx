b5accbec218e48ca71f25c3c12669e4b8e8387f4
Dilum paranavithana <dilumchandupa@gmail.com>
2024-11-18 20:40:55 +0530

    Created individual components for each part of the page and added css.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import JobListings from './components/JobListings'
+import HomeCards from './components/HomeCards'
+import ViewAllJobs from './components/ViewAllJobs'
 
-export default function App() {
+export default function App(){
   return (
-    <div className='text-8xl'>App</div>
+    <div>
+    <Navbar/>
+    <Hero/>
+    <HomeCards/>
+    <JobListings/>
+    <ViewAllJobs/>
+    
+</div>
   )
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Card({children, bg}) {
+  return (
+    <div className={`${bg} p-6 rounded-lg shadow-md`}>{children}</div>
+  )
+}
+
+export default Card
