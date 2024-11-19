f55717b55e3046d8d2b1b2c34b7521dfae0cdd21
Dilum paranavithana <dilumchandupa@gmail.com>
2024-11-19 18:03:00 +0530

    Made the jobs page to get data from backend.

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -4,7 +4,23 @@ import { useEffect,useState } from 'react';
 
 
 function JobListings({isHome=false}) {
-    const JobListings = isHome ? jobs.slice(0,3) : jobs;
+    const [jobs,setJobs] = useState([]);
+    const [loading,setLoading] = useState(true);
+
+    useEffect(() => {
+      const fetchJobs = async () => {
+        try {
+          const res = await fetch('http://localhost:8000/jobs')
+          const data = await res.json();
+          setJobs(data);
+        } catch (error) {
+          console.log('Error fetching Data', error);
+        }finally{
+          setLoading(false);
+        }
+      }
+      fetchJobs();
+    }, []);
   return (
     <div>
     <section className="bg-blue-50 px-4 py-10">
@@ -13,7 +29,7 @@ function JobListings({isHome=false}) {
           {isHome ? 'Recent Jobs': 'Browse Jobs'}
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {JobListings.map((job)=>(
+          {jobs.map((job)=>(
                     <JobListing key={job.id} job={job}/>
           ))}
         </div>
